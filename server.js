5f5eb1065272bcd21ce26997adb76097f77f0e15
ADITYA R RUDRA <adityaberasia@gmail.com>
2021-06-25 16:57:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const server = new ApolloServer({
     typeDefs,
     resolvers,
     introspection: true,
-    playground: true
+    playground: true,
     context: ({ req, res }) => {
         const user = {};
         const token = req.get('Authorization') || '';
